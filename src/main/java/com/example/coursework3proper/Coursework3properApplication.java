0d492dee5f69ecb20be01b0d9e954ddeae3af676
dbhor <dbhor1000@gmail.com>
2023-03-28 15:15:28 +0300

    Initial commit

---
+++ src/main/java/com/example/coursework3proper/Coursework3properApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursework3proper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework3properApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework3properApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coursework3proper/Coursework3properApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework3proper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework3properApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
